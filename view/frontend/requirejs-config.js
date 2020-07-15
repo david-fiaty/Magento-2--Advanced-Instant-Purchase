d78e1ef12374474b0bf3ea48bd8e3cad4977f5fd
david-fiaty-cko <david.fiaty@checkout.com>
2020-07-15 19:50:33 +0100

    Added the frontend view files

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,27 @@
+/**
+ * Naxero.com
+ * Professional ecommerce integrations for Magento
+ *
+ * PHP version 7
+ *
+ * @category  Magento2
+ * @package   Naxero
+ * @author    Platforms Development Team <contact@naxero.com>
+ * @copyright Naxero.com
+ * @license   https://opensource.org/licenses/mit-license.html MIT License
+ * @link      https://www.naxero.com
+ */
+
+ var config = {
+    map: {
+        '*': {
+            aiicore:  'Naxero_AdvancedInstantPurchase/js/core',
+        }
+    },
+    shim: {
+        tabulator: {
+            deps: ['jquery', 'jquery/ui']
+        }
+    },
+    urlArgs: "bust=" + (new Date()).getTime()
+};
---
+++ view/frontend/web/js/core.js
@@ -0,0 +1,30 @@
+/**
+ * Naxero.com
+ * Professional ecommerce integrations for Magento
+ *
+ * PHP version 7
+ *
+ * @category  Magento2
+ * @package   Naxero
+ * @author    Platforms Development Team <contact@naxero.com>
+ * @copyright Naxero.com
+ * @license   https://opensource.org/licenses/mit-license.html MIT License
+ * @link      https://www.naxero.com
+ */
+
+define(
+    [
+        'jquery',
+        'aiicore',
+        'mage/translate'
+    ],
+    function ($, core, __) {
+        'use strict';
+        
+        return {
+            load: function () {
+                return this;
+            }
+        };
+    }
+);
