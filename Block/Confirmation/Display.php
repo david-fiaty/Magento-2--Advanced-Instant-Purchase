db751db87e575448e3db23b57ca55126ae88ba4e
david-fiaty-cko <david.fiaty@checkout.com>
2020-09-06 04:33:35 +0100

    Updated the product display

--- Block/Confirmation/Display.php
+++ Block/Confirmation/Display.php
@@ -12,15 +12,22 @@ class Display extends \Magento\Framework\View\Element\Template
     public $customerHelper;
 
     /**
+     * @var Config
+     */
+    public $configHelper;
+
+    /**
      * Display constructor.
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Naxero\AdvancedInstantPurchase\Helper\Customer $customerHelper,
+        \Naxero\AdvancedInstantPurchase\Helper\Config $configHelper,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->customerHelper = $customerHelper;
+        $this->configHelper = $configHelper;
     }
 
     /**
@@ -29,4 +36,12 @@ class Display extends \Magento\Framework\View\Element\Template
     public function getConfirmContent() {
         return $this->customerHelper->getConfirmContent();
     }
+
+    /**
+     * Get the module config.
+     */
+    public function getConfig() {
+        return $this->configHelper->getValues();
+    }
+
 }
