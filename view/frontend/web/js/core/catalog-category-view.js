7d3ceb52b7e8f04f2c367bd2a8d841f0485a8235
David Fiaty <dfiaty@gmail.com>
2020-09-23 23:35:01 +0100

    Structure and code improvements

--- view/frontend/web/js/core/catalog-category-view.js
+++
@@ -1,9 +0,0 @@
-require([
-    'jquery', 'domReady!'
-], function($) {
-        $(document).on('click','.aip-button',function(e) {
-
-            console.log($(e.currentTarget).attr('id'));
-
-        });
-});
