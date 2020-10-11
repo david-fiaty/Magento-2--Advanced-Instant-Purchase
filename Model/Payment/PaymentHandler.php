e295026788a219bec88a6c1c4f3d777304aa744c
David Fiaty <dfiaty@gmail.com>
2020-10-11 23:23:07 +0100

    Code improvements

--- Model/Payment/PaymentHandler.php
+++ Model/Payment/PaymentHandler.php
@@ -3,19 +3,35 @@ namespace Naxero\AdvancedInstantPurchase\Model\Payment;
 
 class PaymentHandler
 {
+    /**
+     * Load a payment integration instance.
+     */
     public function loadMethod($code) {
-        $classPath = $this->getClassPath($code);
+        $classPath = $this->getIntegrationPath($code);
         return new $classPath(); 
     }
 
-    public function getClassPath($code) {
+    /**
+     * Get the payment integration path.
+     */
+    public function getIntegrationPath($code) {
         $parts = explode('_', strtolower($code));
         $path = "\\Naxero\\AdvancedInstantPurchase\\Model\\Payment\\Integration\\";
-        foreach ($parts as $part) {
-            $path .= ucfirst($part);
-        }
+        $path .= $this->getIntegrationName($parts);
         $path .= "\\Index";
 
         return $path;
+    }
+
+    /**
+     * Get the payment integration name.
+     */
+    public function getIntegrationName($parts) {
+        $name = '';
+        foreach ($parts as $part) {
+            $name .= ucfirst($part);
+        }
+
+        return $name;
     }
 }
