8facdfb3eb8f5529110894e33c461a286b9fb046
David Fiaty <dfiaty@gmail.com>
2020-10-11 21:05:15 +0100

    Upadte an interface methods

--- Model/Payment/PaymentHandlerInterface.php
+++ Model/Payment/PaymentHandlerInterface.php
@@ -4,9 +4,9 @@ namespace Naxero\AdvancedInstantPurchase\Model\Payment;
 interface PaymentHandlerInterface
 {
 
-    public function sendRequest($quote);
+    public function sendRequest();
 
-    public function getResponse($order, $response);
+    public function getResponse();
 
     public function isSuccess();
 
