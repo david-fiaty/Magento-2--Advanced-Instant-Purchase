f30f1842736b14056f1f685aa2e49d58ddc2f47d
David Fiaty <dfiaty@gmail.com>
2020-10-12 02:16:56 +0100

    Added code comments

--- Model/Payment/Integration/PaymentIntegrationInterface.php
+++ Model/Payment/Integration/PaymentIntegrationInterface.php
@@ -1,11 +1,29 @@
 <?php
 namespace Naxero\AdvancedInstantPurchase\Model\Payment\Integration;
 
+/**
+ * Interface PaymentIntegrationInterface.
+ */
 interface PaymentIntegrationInterface
 {
+    /**
+     * Send a payment request.
+     *
+     * @param Quote $quote
+     * @param array $paymentData
+     */
     public function sendRequest($quote, $paymentData);
 
+    /**
+     * Create an order.
+     *
+     * @param Quote $quote
+     * @param string|array|object $paymentResponse
+     */
     public function createOrder($quote, $paymentResponse);
 
+    /**
+     * Check if a payment is successful.
+     */
     public function paymentSuccess();
 }
