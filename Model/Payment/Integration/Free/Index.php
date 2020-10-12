7912751dc3aa9f67fb6251a16ab198e0546bd216
David Fiaty <dfiaty@gmail.com>
2020-10-12 02:57:22 +0100

    Added paypal billing and free integrations

---
+++ Model/Payment/Integration/Free/Index.php
@@ -0,0 +1,56 @@
+<?php
+namespace Naxero\AdvancedInstantPurchase\Model\Payment\Integration\Free;
+
+/**
+ * Class Index.
+ */
+class Free implements \Naxero\AdvancedInstantPurchase\Model\Payment\Integration\PaymentIntegrationInterface
+{
+    /**
+     * @var QuoteManagement
+     */
+    public $quoteManagement;
+
+    /**
+     * @var string|array|object
+     */
+    public $response;
+
+    /**
+     * Class Index constructor.
+     */
+    public function __construct(
+        \Magento\Quote\Model\QuoteManagement $quoteManagement
+    ) {
+        $this->quoteManagement = $quoteManagement;
+    }
+
+    /**
+     * Send a payment request.
+     */
+    public function sendRequest($quote, $paymentData) {
+        // Get the payment response
+        $response = '';
+
+        // Assign the response to self
+        $this->response = $response;
+
+        return $this;
+    }
+
+    /**
+     * Chcek if a payment request is successful.
+     */
+    public function paymentSuccess() {
+        return true;
+    }
+
+    /**
+     * Create a new order.
+     */
+    public function createOrder($quote, $paymentResponse) { 
+        $order = $this->quoteManagement->submit($quote);
+
+        return $order;
+    }
+}
