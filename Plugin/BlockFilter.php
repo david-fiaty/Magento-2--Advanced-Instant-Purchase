c66a9322d905da3f1c5e1fef2e5e8a677ab423e6
David Fiaty <dfiaty@gmail.com>
2020-10-18 00:12:04 +0100

    Code improvements

--- Plugin/BlockFilter.php
+++ Plugin/BlockFilter.php
@@ -5,30 +5,33 @@ namespace Naxero\AdvancedInstantPurchase\Plugin;
 class BlockFilter
 {
     public function afterToHtml($subject, $html) {
-        // Get all button tags in content
-        $validation = '/\{BuyNow\}/i';
-        //$tag = '{BuyNow}';
+        // Prepare the tag search pattern
+        $search = '/\{BuyNow\}/i';
+
+        // Find all matches
         preg_match_all(
-            $validation,
+            $search,
             $html,
             $tags
         );
 
-        // Replace the tags
-        //foreach ($tags as $tag) {
+        // Loop through the found tags
+        $condition = !empty($tags) && strpos($className, '\\BlockButton\\') === false;
+        if ($condition) {
+            foreach ($tags as $tag) {
+                // Get the class name
+                $className = get_class($subject);
 
-            $className = get_class($subject);
-            if (strpos($className, '\\BlockButton\\') === false) {
+                // Process the blocks output
                 $blockHtml = $subject->getLayout()
                 ->createBlock('Naxero\AdvancedInstantPurchase\Block\Button\BlockButton')
                 ->setTemplate('Naxero_AdvancedInstantPurchase::button/block.phtml')
                 ->toHtml();
 
+                // Replace the tag with the generated HTML
                 $html = str_replace($tags[0], $blockHtml, $html);
             }
-        
-
-        //}
+        }
 
         return $html;
     }
