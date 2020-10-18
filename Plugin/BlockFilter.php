4762c315de348942f4e040599e02844d035e3e39
David Fiaty <dfiaty@gmail.com>
2020-10-18 20:30:07 +0100

    Code improvements

--- Plugin/BlockFilter.php
+++ Plugin/BlockFilter.php
@@ -5,6 +5,43 @@ namespace Naxero\AdvancedInstantPurchase\Plugin;
 class BlockFilter
 {
     public function afterToHtml($subject, $html) {
+        $matches = $this->getBlockTags($subject, $html);
+        if ($matches) {
+            for ($i = 0; $i < count($matches[0]); $i++) {
+                // Prepare the product block
+                $blockHtml = $subject->getLayout()
+                ->createBlock('Naxero\AdvancedInstantPurchase\Block\Button\BlockButton')
+                ->setTemplate('Naxero_AdvancedInstantPurchase::button/base.phtml');
+
+                // Loop through the tag parameters
+                if ($this->hasParameters($matches, $i)) {
+                    // Product id
+                    preg_match('/product_id=(\d*)/', $matches[1][$i], $param);
+
+                    $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
+$logger = new \Zend\Log\Logger();
+$logger->addWriter($writer);
+$logger->info(var_dump($param));
+
+                    $valid = isset($param[1]) && !empty($param[1]) && (int) $param[1] > 0;
+                    if ($valid) {
+                        $blockHtml->setData('product_id', $param[1]);
+                    } 
+                }
+
+                // Replace the tag with the generated HTML
+                $html = str_replace($matches[1][$i], $blockHtml->toHtml(), $html);
+            }
+
+        }
+
+        return $html;
+    }
+
+    public function getBlockTags($subject, $html) {
+        // Get the target class name
+        $className = get_class($subject);
+
         // Prepare the tag search pattern
         $search = '/\{BuyNow(.*)\}/i';
 
@@ -14,51 +51,19 @@ class BlockFilter
             $html,
             $matches
         );
-        
-        // Get the target class name
-        $className = get_class($subject);
 
         // Loop through the found tags
         $condition = !empty($matches) && !empty($matches[0])
         && strpos($className, '\\BlockButton\\') === false
-        && is_array($matches[0]);
-        if ($condition) {
-            for ($i = 0; $i < count($matches[0]); $i++) {
-                // Prepare the product block
-                $blockHtml = $subject->getLayout()
-                ->createBlock('Naxero\AdvancedInstantPurchase\Block\Button\BlockButton')
-                ->setTemplate('Naxero_AdvancedInstantPurchase::button/base.phtml');
+        && is_array($matches[0])
+        && count($matches[0]) > 0;
 
-                // Loop through the tag parameters
-                $hasParameters = isset($matches[1])
-                && isset($matches[1][$i]) && !empty($matches[1][$i]);
-
-                if ($hasParameters) {
-                    // Product id
-
-                    /*
-                    $valid = isset($param[1]) && !empty($param[1]) && (int) $param[1] > 0;
-                    var_dump($param);
-
-var_dump($valid);
-*/
-                    /*
-                    preg_match('/product_id=(\d*)/', $matches[1][$i], $param);
-                    $valid = isset($param[1]) && !empty($param[1]) && (int) $param[1] > 0;
-                    if ($valid) {
-                        //$subject->setData('product_id', $param[1]);
-                    } 
-                    */
-                }
-
-                // Render the block
-                         $subject->setData('product_id', 6);
-
-                // Replace the tag with the generated HTML
-                $html = str_replace($matches[1][$i], $blockHtml->toHtml(), $html);
-            }
-        }
-
-        return $html;
+        return $condition ? $matches : null;
     }
+
+    public function hasParameters($matches, $i) {
+        return isset($matches[1])
+        && isset($matches[1][$i]) && !empty($matches[1][$i]);
+    }
+
 } 
