e44f996505ae857b02b5a59d9b28a976fa06d494
David Fiaty <dfiaty@gmail.com>
2020-10-18 22:00:28 +0100

    Improved the block tags parameters handling code

--- Plugin/BlockFilter.php
+++ Plugin/BlockFilter.php
@@ -31,31 +31,30 @@ class BlockFilter
         // Process tags found
         if ($matches) {
             for ($i = 0; $i < count($matches[0]); $i++) {
-                // Error count for each tag found
+                // Prepare the loop variables
                 $errors = 0;
+                $tag = $matches[0][$i];
 
                 // Loop through the tag parameters
                 if ($this->tagHasParameters($matches, $i)) {
                     // Product id
                     preg_match('/product_id=(\d*)/', $matches[1][$i], $param);
-                    $valid = isset($param[1]) && !empty($param[1]) && (int) $param[1] > 0;
-                    if ($valid) {
+                    if ($this->isParameterValid($param)) {
                         // Build the block
                         $blockHtml = $this->blockHelper->buildButtonBlock($subject);
 
                         // Set the product id block parameter
                         $blockHtml->setData('product_id', $param[1]);
-
-                        // Replace the tag with the generated HTML
-                        $html = str_replace($matches[1][$i], $blockHtml->toHtml(), $html);
                     } 
                     else {
                         $errors++;
                     }
-                }
 
-                // Handle the errors
-                //$errors;
+                    // Replace the tag with the generated HTML
+                    if ($errors == 0) {
+                        $html = str_replace($tag, $blockHtml->toHtml(), $html);
+                    }
+                }
             }
         }
 
@@ -65,7 +64,14 @@ class BlockFilter
     /**
      * Check if a block tag has parameters.
      */
-    public function tagHasParameters($matches, $i) {
+    public function tagHasParameters(array $matches, int $i) {
         return isset($matches[1]) && isset($matches[1][$i]) && !empty($matches[1][$i]);
+    }
+
+    /**
+     * Check if a tag parameter is valid.
+     */
+    public function isParameterValid(array $param) {
+        return isset($param[1]) && !empty($param[1]) && (int) $param[1] > 0;
     }
 } 
