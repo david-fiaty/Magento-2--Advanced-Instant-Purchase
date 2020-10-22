34967cf5db49a12fffcd243dcc41e5896df067a9
David Fiaty <dfiaty@gmail.com>
2020-10-23 00:58:10 +0100

    Added a base button widget

---
+++ Block/Button/WidgetButton.php
@@ -0,0 +1,15 @@
+<?php
+namespace Naxero\AdvancedInstantPurchase\Block\Button;
+
+/**
+ * WidgetButton class constructor.
+ */
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class WidgetButton extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
+{
+
+    protected $_template = "button/test.phtml";
+
+}
