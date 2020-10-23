5445c1a5695415cce4c54788b9bf2587b3b5454b
David Fiaty <dfiaty@gmail.com>
2020-10-23 05:23:55 +0100

    Added a naming source static class

--- Helper/Config.php
+++ Helper/Config.php
@@ -1,6 +1,6 @@
 <?php
 namespace Naxero\AdvancedInstantPurchase\Helper;
-
+v
 /**
  * Class Config helper.
  */
---
+++ Model/Config/Naming.php
@@ -0,0 +1,13 @@
+<?php
+namespace Naxero\AdvancedInstantPurchase\Model\Config;
+
+/**
+ * Class Naming.
+ */
+class Naming
+{
+
+    public static function getModulename() {
+        return 'Naxero_AdvancedInstantPurchase';
+    }
+} 
