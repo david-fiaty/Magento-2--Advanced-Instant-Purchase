d4f70ab67b732784a757a0584fe19163b305af11
David Fiaty <dfiaty@gmail.com>
2020-10-23 03:18:10 +0100

    Update ProductList.php

--- Model/Config/Backend/Source/ProductList.php
+++ Model/Config/Backend/Source/ProductList.php
@@ -7,21 +7,38 @@ namespace Naxero\AdvancedInstantPurchase\Model\Config\Backend\Source;
 class ProductList implements \Magento\Framework\Option\ArrayInterface
 {
     /**
+     * CollectionFactory
+     */
+    public $productCollectionFactory;
+        
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory       
+    ) {    
+        $this->productCollectionFactory = $productCollectionFactory;    
+    }
+    
+    public function getProducts()
+    {
+        $items = [];
+        $collection = $this->productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        foreach ($collection as $item) {
+            $items[] = [
+                'value' => $item->getId(),
+                'label' => __($item->getName())
+            ];
+        }
+
+        return $items;
+    }
+
+    /**
      * Options getter
      *
      * @return array
      */
     public function toOptionArray()
     { 
-        return [
-            [
-                'value' => 1,
-                'label' => __('A')
-            ],
-            [
-                'value' => 0,
-                'label' => __('B')
-            ]
-        ];
+        return $this->getProducts();
     }
 }
