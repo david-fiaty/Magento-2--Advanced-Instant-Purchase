a985281d4458b5e5c294cc1d162b29de2a8180dd
David Fiaty <dfiaty@gmail.com>
2020-10-25 22:46:06 +0000

    Update logger.js

--- view/frontend/web/js/view/helpers/logger.js
+++ view/frontend/web/js/view/helpers/logger.js
@@ -1,50 +1,71 @@
 define([
     'mage/translate',
+    'Naxero_AdvancedInstantPurchase/js/view/helpers/modal',
     'Naxero_AdvancedInstantPurchase/js/view/helpers/tree'
-], function (__, AipTree) {
+], function (__, AipModal, AipTree) {
     'use strict';
 
     return {
         logCount: 1,
+        logTitleCss: 'font-weight: bold; color: blue;',
 
         /**
          * Log data to the browser console.
-         *
-         * @param {Object} data
          */
         log: function(obj, msg, data) {
             // Default data value
             data = data || null;
 
-            // Check the logging settings
-            var condition = obj.jsConfig.general.debug_enabled
-            && obj.jsConfig.general.console_logging_enabled;
-
             // Handle the logging display
-            if (condition) {
-                // Module name
-                console.log(
-                    '%c[' + this.logCount + '][' + obj.jsConfig.module.title + ']',
-                    'font-weight: bold; color: blue;'
-                );
-
-                // Log event title
-                console.log(msg)
-
-                // Log event data
-                if (data) { 
-                    console.log(data);
-                }
-
-                // Log count
-                this.logCount++;
+            if (this.isConsoleLoggingEnabled(obj)) {
+                this.logToConsole(obj, msg, data);
             }
         },
 
         /**
+         * Handle the data console logging logic.
+         */
+        logToConsole: function(obj, msg, data) {
+            // Log title
+            console.log(
+                this.getLogTitle(obj),
+                this.logTitleCss
+            );
+                
+            // Log the event message
+            console.log(msg)
+
+            // Log event data
+            if (data) console.log(data);
+
+            // Log count
+            this.logCount++;
+        },
+ 
+        /**
+         * Check if console logging is enabled.
+         */
+        isConsoleLoggingEnabled: function(obj) {
+            return obj.jsConfig.general.debug_enabled
+            && obj.jsConfig.general.console_logging_enabled;
+        },
+
+        /**
+         * Get a log title.
+         */
+        getLogTitle: function(obj) {
+            return '%c[' + this.logCount + '][' + obj.jsConfig.module.title + ']';
+        },
+
+        /**
+         * Show the UI logger in a modal.
+         */
+        showUiLogger: function(obj) {
+            AipTree.build(obj);
+        },
+
+        /**
          * Build a browsable tree with log data.
-         *
-         * @param {Object} data
          */
         buildDataTree: function(obj) {
             AipTree.build(obj);
