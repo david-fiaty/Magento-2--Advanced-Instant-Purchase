74260dc5c84eb3c4157689b053f240ac635e1c73
David Fiaty <dfiaty@gmail.com>
2020-11-02 00:18:38 +0000

    Added a missing php class dependency

--- Block/Screen/Confirmation.php
+++ Block/Screen/Confirmation.php
@@ -7,12 +7,20 @@ namespace Naxero\BuyNow\Block\Screen;
 class Confirmation extends \Magento\Framework\View\Element\Template
 {
     /**
+     * @var Block
+     */
+    public $blockHelper;
+
+    /**
      * ViewButton class constructor.
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        \Naxero\BuyNow\Helper\Block $blockHelper,
         array $data = []
     ) {
         parent::__construct($context, $data);
+
+        $this->blockHelper = $blockHelper;
     }
 }
