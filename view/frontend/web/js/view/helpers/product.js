297cc602b688a902af1b943aa4b4cf6ffece663e
David Fiaty <dfiaty@gmail.com>
2020-11-10 01:07:07 +0000

    Bug fix

--- view/frontend/web/js/view/helpers/product.js
+++ view/frontend/web/js/view/helpers/product.js
@@ -86,6 +86,7 @@
          * Update the selected product options values.
          */
         updateSelectedOptionsValues: function (e, obj) {
+            console.log(obj);
             var condition1 = this.hasOptions(e);
             var condition2 = obj.jsConfig.blocks.show_product && NbnView.isBlockView();
             var condition3 = !NbnView.isBlockView();
