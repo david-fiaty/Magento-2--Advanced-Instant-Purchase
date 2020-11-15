211988e2f2d4f8ebed8f4288e4a7f2019e7de489
David Fiaty <dfiaty@gmail.com>
2020-11-15 22:48:00 +0000

    Update

--- view/frontend/web/js/view/helpers/validation.js
+++ view/frontend/web/js/view/helpers/validation.js
@@ -12,17 +12,14 @@
  * @link      https://www.naxero.com
  */
 
- define(
-    [
+ define([
         'jquery',
         'mage/translate'
     ],
     function ($, __) {
         'use strict';
         return {
-            agreementRow: '.nbn-agreement-link-row',
-            agreementBoxSelector: '.nbn-agreement-box',
-            inputSelectors: '.nbn-select, .nbn-box',
+            regionSelector: '#region_id',
 
             /**
              * Initialise the object.
@@ -33,48 +30,16 @@
             },
 
             /**
-             * Additional form validation.
-             */
-            validate: function () {
-                // Prepare the parameters
-                var errors = [];
-
-                // Agreements validation
-                if (this.o.jsConfig.general.enable_agreements) {
-                    $(this.agreementRow).removeClass('error');
-                    $(this.agreementRow).each(function () {
-                        var input = $(this).find(this.agreementBoxSelector);
-                        if (!input.is(':checked')) {
-                            errors.push({
-                                id: input.attr('id')
-                            });
-                        }
-                    });
-                }
-
-                // Fields validation
-                $(this.inputSelectors).each(function () {
-                    var val = $(this).val();
-                    if (val && val.length == 0) {
-                        errors.push({
-                            id: input.attr('id')
-                        });
-                    }
-                });
-
-                return errors.length == 0;
-            },
-
-            /**
              * Check the region state in address form.
              */
             checkRegionState: function () {
-                if ($('#region_id').prop('disabled') === true) {
-                    $('#region_id').addClass('nbn-region-hidden');
-                    $('#region_id').removeClass('nbn-region-visible');
+                var regionField = $(this.regionSelector);
+                if (regionField.prop('disabled') === true) {
+                    regionField.addClass('nbn-region-hidden');
+                    regionField.removeClass('nbn-region-visible');
                 } else {
-                    $('#region_id').addClass('nbn-region-visible');
-                    $('#region_id').removeClass('nbn-region-hidden');
+                    regionField.addClass('nbn-region-visible');
+                    regionField.removeClass('nbn-region-hidden');
                 }
             }
         }
