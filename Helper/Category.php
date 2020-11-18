7e1cb8e23cb87e5f0f9e68df245038f57c21e0fb
David Fiaty <dfiaty@gmail.com>
2020-11-18 04:19:24 +0000

    Create Category.php

---
+++ Helper/Category.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Naxero.com
+ * Professional ecommerce integrations for Magento.
+ *
+ * PHP version 7
+ *
+ * @category  Magento2
+ * @package   Naxero
+ * @author    Platforms Development Team <contact@naxero.com>
+ * @copyright © Naxero.com all rights reserved
+ * @license   https://opensource.org/licenses/mit-license.html MIT License
+ * @link      https://www.naxero.com
+ */
+
+namespace Naxero\BuyNow\Helper;
+
+/**
+ * Class Category helper.
+ */
+class Category extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * Class Category helper constructor.
+     */
+    public function __construct(
+     
+    ) {
+ 
+    }
+
+
+}
