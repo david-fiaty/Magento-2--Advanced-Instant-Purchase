5491a1b7a5123e4da60eef7e734b9901a90e2cc8
David Fiaty <dfiaty@gmail.com>
2020-11-28 01:48:24 +0000

    Update quantity-box.js

--- view/frontend/web/js/view/assets/quantity-box.js
+++ view/frontend/web/js/view/assets/quantity-box.js
@@ -42,28 +42,20 @@ define([
             }),
 
             // Click event
-            $(document).on('click', '.plus, .minus', function () {
-                var a = $(this).closest('.quantity').find('.qty'),
-                    b = parseFloat(a.val()),
-                    c = parseFloat(a.attr('max')),
-                    d = parseFloat(a.attr('min')),
-                    e = a.attr('step');
-                (b && '' !== b && 'NaN' !== b) || (b = 0),
-                    ('' !== c && 'NaN' !== c) || (c = ''),
-                    ('' !== d && 'NaN' !== d) || (d = 0),
-                    ('any' !== e &&
-                        '' !== e &&
-                        void 0 !== e &&
-                        'NaN' !== parseFloat(e)) ||
-                        (e = 1),
-                    $(this).is('.plus')
-                        ? c && b >= c
-                            ? a.val(c)
-                            : a.val((b + parseFloat(e)).toFixed(e.getDecimals()))
-                        : d && b <= d
-                        ? a.val(d)
-                        : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())),
-                    a.trigger('change');
+            $(document).on('click touch', '.plus, .minus', function () {
+                var a = $(this).closest('.quantity').find('.qty');
+                var b = self.prepareValue(a.val(), 0);
+                var c = self.prepareValue(a.attr('max'), '');
+                var d = self.prepareValue(a.attr('min'), 0);
+                var e = self.prepareStepValue(a.attr('step'), 1);
+                $(this).is('.plus')
+                    ? c && b >= c
+                        ? a.val(c)
+                        : a.val((b + parseFloat(e)).toFixed(e.getDecimals()))
+                    : d && b <= d
+                    ? a.val(d)
+                    : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())),
+                a.trigger('change');
             });
         },
 
@@ -78,6 +70,23 @@ define([
                 c.children().first().before('<input type="button" value="-" class="minus" />');
                 c.children().last().after('<input type="button" value="+" class="plus" />');
             });
+        },
+
+        /**
+         * Prepare a quantity box value.
+         */  
+        prepareValue: function (val, defaultValue) {
+            val = parseFloat(val);
+            return val && val !== '' && val !== 'NaN'
+            ? val : defaultValue;
+        },
+
+        /**
+         * Prepare a quantity box step value.
+         */  
+        prepareStepValue: function (val, defaultValue) {
+            return val !== 'any' && val !== '' && val !== void 0 && parseFloat(val) !== 'NaN'
+            ? val : defaultValue;
         }
     }
 });
