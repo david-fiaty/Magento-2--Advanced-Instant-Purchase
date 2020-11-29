d633957fe25508e01c4ca39beb4be2c7984dcf1f
David Fiaty <dfiaty@gmail.com>
2020-11-29 00:08:06 +0000

    Improved the quantity box value handling

--- view/frontend/web/js/view/assets/quantity-box.js
+++ view/frontend/web/js/view/assets/quantity-box.js
@@ -18,80 +18,44 @@ define([
 ], function ($) {
     'use strict';
 
-    // Extend the decimals prototype
-    String.prototype.getDecimals || (String.prototype.getDecimals = function () {
-        var a = this,
-            b = ('' + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-        return b
-            ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0))
-            : 0;
-    });
-
     return {
         /**
          * Build a product quantity box.
          */  
         build: function(boxId) {
-            // Refresh quantity increments
-            this.refresh(boxId);
+            // Prepare variables
+            var quantityField = $('#' + boxId + ' .nbn-qty');
+            var min = parseInt(quantityField.attr('min'));
+            var max = parseInt(quantityField.attr('max'));
+            var step = parseInt(quantityField.attr('step'));
 
-            // Update event
-            var self = this;
-            $(document).on('updated_wc_div', function () {
-                self.refresh(boxId);
+            // Value change event
+            quantityField.on('change', function (e) {
+                if ($(e.currentTarget).hasClass('nbn-qty')) {
+                    var currentValue = parseInt($(this).val());
+                    var condition = currentValue && currentValue !== 'Nan' && currentValue > 0;
+                    var newValue = condition ? currentValue : min;
+                    $(this).val(newValue);
+                }
             });
 
-            // Prepare the quantity button selectors
-            var selectors = '#' + boxId + ' ' + '.nbn-qty-minus';
-            selectors += ', ';
-            selectors += '#' + boxId + ' ' + '.nbn-qty-plus';
-
-            // Click event
-            $(selectors).on('click touch', function (e) {
-                var a = $(e.currentTarget).closest('.nbn-qty-box').find('.nbn-qty');
-                var b = self.prepareValue(a.val(), 0);
-                var c = self.prepareValue(a.attr('max'), '');
-                var d = self.prepareValue(a.attr('min'), 0);
-                var e = self.prepareStepValue(a.attr('step'), 1);
-                $(this).is('.nbn-qty-plus')
-                    ? c && b >= c
-                        ? a.val(c)
-                        : a.val((b + parseFloat(e)).toFixed(e.getDecimals()))
-                    : d && b <= d
-                    ? a.val(d)
-                    : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())),
-                a.trigger('change');
+            // Minus button event
+            $('#' + boxId + ' .nbn-qty-minus').on('click touch', function () {
+                var quantity = parseInt(quantityField.val());
+                var newQuantity = quantity - step;
+                var condition = newQuantity && newQuantity !== 'Nan' && newQuantity >= min;
+                newQuantity = condition ? newQuantity : min;
+                quantityField.val(newQuantity);
             });
-        },
 
-        /**
-         * Refresh the quantity increments.
-         */  
-        refresh: function (boxId) {
-            $('#' + boxId + ':not(.nbn-qty-buttons-added), td' + '#' + boxId + ':not(.nbn-qty-buttons-added)')
-            .each(function (a, b) {
-                var c = $(b);
-                c.addClass('nbn-qty-buttons-added');
-                c.children().first().before('<input type="button" value="-" class="nbn-qty-minus" />');
-                c.children().last().after('<input type="button" value="+" class="nbn-qty-plus" />');
+            // Plus button event
+            $('#' + boxId + ' .nbn-qty-plus').on('click touch', function () {
+                var quantity = parseInt(quantityField.val());
+                var newQuantity = quantity + step;
+                var condition = newQuantity && newQuantity !== 'Nan' && newQuantity <= max;
+                newQuantity = condition ? newQuantity : max;
+                quantityField.val(newQuantity);
             });
-        },
-
-        /**
-         * Prepare a quantity box value.
-         */  
-        prepareValue: function (val, defaultValue) {
-            val = parseFloat(val);
-            return val && val !== '' && val !== 'NaN'
-            ? val : defaultValue;
-        },
-
-        /**
-         * Prepare a quantity box step value.
-         */  
-        prepareStepValue: function (val, defaultValue) {
-            return val !== 'any' && val !== '' && val !== void 0 && parseFloat(val) !== 'NaN'
-            ? val : defaultValue;
         }
     }
 });
