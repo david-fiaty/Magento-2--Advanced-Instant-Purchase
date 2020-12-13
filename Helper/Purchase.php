26826ab8269ef83eccc97f9008d723f9249815e0
David Fiaty <dfiaty@gmail.com>
2020-12-13 19:51:46 +0000

    UPdate

--- Helper/Purchase.php
+++ Helper/Purchase.php
@@ -223,6 +223,17 @@ class Purchase extends \Magento\Framework\App\Helper\AbstractHelper
         $isGroupValid = $this->customerHelper->canDisplayForGroup();
         $isTimeValid = $this->isProductTimeLimitValid();
         
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/a.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        $logger->info(print_r($buttonEnabled, 1));
+        $logger->info(print_r($isGroupValid, 1));
+        $logger->info(print_r($isTimeValid, 1));
+        $logger->info(print_r($isLoggedIn, 1));
+        $logger->info(print_r($showGuestButton, 1));
+
+
+        return true;
         return $buttonEnabled && $isGroupValid && $isTimeValid
         && ($isLoggedIn || $showGuestButton);
     }
