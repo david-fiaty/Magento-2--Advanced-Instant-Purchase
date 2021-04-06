2188c561017b8a6fcc818704b95b9be8c064c6e8
David Fiaty <dfiaty@gmail.com>
2021-04-06 17:05:10 +0100

    Update

--- Block/Popup/Confirmation.php
+++ Block/Popup/Confirmation.php
@@ -29,16 +29,23 @@ class Confirmation extends \Magento\Framework\View\Element\Template
     public $blockHelper;
 
     /**
+     * @var Tools
+     */
+    public $toolsHelper;
+
+    /**
      * Confirmation class constructor.
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Naxero\BuyNow\Helper\Block $blockHelper,
+        \Naxero\BuyNow\Helper\Tools $toolsHelper,
         array $data = []
     ) {
         parent::__construct($context, $data);
 
         $this->blockHelper = $blockHelper;
+        $this->toolsHelper = $toolsHelper;
     }
 
     /**
